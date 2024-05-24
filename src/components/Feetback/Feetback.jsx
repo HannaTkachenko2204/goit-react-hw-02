eed9cdb18614f904eb5562be2087963d89856d26
Анна <anna.kuvakina91@gmail.com>
2024-05-24 17:14:37 +0300

    Feedback

---
+++ src/components/Feetback/Feetback.jsx
@@ -0,0 +1,13 @@
+const Feedback = ({feedback, positiveFeedback, totalFeedback}) => {
+  return (
+    <>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedback}%</p>
+    </>
+  );
+};
+
+export default Feedback;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,7 @@
+const Notification = () => {
+    return (<>
+<p>No feetback yet</p>
+    </>)
+}
+
+export default Notification
